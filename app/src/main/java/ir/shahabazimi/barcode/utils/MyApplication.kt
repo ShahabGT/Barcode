66b913932bfa9c24468436fa9310e509ed76ebda
Shahab <azimishahab@gmail.com>
2022-10-22 21:49:46 +0330

    Initial Commit - Project Created

---
+++ app/src/main/java/ir/shahabazimi/barcode/utils/MyApplication.kt
@@ -0,0 +1,12 @@
+package ir.shahabazimi.barcode.utils
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
